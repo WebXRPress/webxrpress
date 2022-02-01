073c504b4df06d4e86e0df7eaaba55c2fe48c34a
Stephen J. Carnam <steve@steveorevo.com>
2022-02-01 00:34:26 -0800

    Init. header

---
+++ webxrpress.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: WebXRPress
+ * Plugin URI: https://github.com/WebXRPress/webxrpress
+ * Description: An plugin for WordPress that makes embedding and creating virtual reality, (and eventually augmented reality) WebXR based experiences easy.
+ * Version: 0.0.1
+ * Author: Stephen J. Carnam
+ * Author URI: https://carnam.net/about/
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */  
+
